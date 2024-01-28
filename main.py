da448a44f39f03aa77f9e4089fe31a6e0b3968e9
AJPL88 <accelerator5460@gmail.com>
2024-01-28 10:07:25 -0800

    Good version :)
    Signed-off-by: Alan Jiang <accelerator5460@gmail.com>
    Co-authored-by: Blobewab-20 <Blobewab-20@users.noreply.github.com>
    Co-authored-by: Ethan Kou <BubblyBingBong@users.noreply.github.com>
    Co-authored-by: MysticalApple <MysticalApple@users.noreply.github.com>

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from punch_detection import HandPunchDetector
 false = False
 true = True
 asdp = [false, false, false, false]
-difficulty = 2000 # high number: less difficult
+difficulty = EASY # high number: less difficult
 # 2000: easy
 # 1300: medium
 # 700: hard
@@ -34,25 +34,49 @@ except KeyboardInterrupt:
 
 #positions = left, right, middle
 #actions = punching, resting
-
+class physicsObj():
+    def __init__(self):
+        self.imagePath = ""
+        self.pos = (0,0)
+        self.velocity = (0,0)
+        self.acceleration = (0,0)
+    def setInit(self, newImg, newPos,newVelocity, newAccl):
+        self.imagePath=newImg
+        self.pos=newPos
+        self.velocity=newVelocity
+        self.acceleration=newAccl
+    def update(self):
+        self.pos[0]+=self.velocity[0] * ticks/TICK_PHYSICS_MULTIPLIER
+        self.pos[1]+=self.velocity[1]* ticks/TICK_PHYSICS_MULTIPLIER
+        self.velocity[0]+=self.acceleration[0]* ticks/TICK_PHYSICS_MULTIPLIER
+        self.velocity[1]+=self.acceleration[1]* ticks/TICK_PHYSICS_MULTIPLIER
+    def display(self):
+        if self.imagePath == "": return
+        screen.blit(self.imagePath, (self.pos[0],self.pos[1]))
+        
 class Character():
     def __init__(self):
+        self.isAllied = 1
         self.xpos = 0 #range (-.5) - (.5)
-        self.healthPoints = DEFAULT_HP
+        self.healthPoints = ALLIED_HP
         self.action = REST
         self.cooldown = 0
         self.positionTime = 0 #how long your character has been in the same positionpygame.time.get_ticks()
-
+    def setInitState (self,alliedState, Health):
+        self.isAllied=alliedState
+        self.healthPoints=Health
     def update(self, newxpos, newAction): # reads the action and position from CV and uses them to update the player
         currentTime = pygame.time.get_ticks()
         self.xpos = newxpos # updates position (no cooldown)
-       
         if newAction == REST:
             self.action = REST
         else: #new action = punch
             if self.cooldown <= 0:
                 self.action = PUNCH
-                self.cooldown = DEFAULT_PUNCH_CD
+                if(self.isAllied):
+                    self.cooldown = ALLIED_COOLDOWN
+                else:
+                    self.cooldown = ENEMY_COOLDOWN
             else:
                 self.action = REST
 
@@ -62,6 +86,8 @@ class Character():
 #game objects
 enemyChar = Character()
 youChar = Character()
+enemyChar.setInitState(false,ENEMY_HP)
+youChar.setInitState(true,ALLIED_HP)
 
 testplayerpos = 0
 
@@ -76,28 +102,13 @@ def playerPosition():
 
  
 def enemyAction(): # done
-    if enemyChar.cooldown <= 0 and abs(enemyChar.xpos-youChar.xpos)<=ENEMY_TARGET_RANGE and youChar.positionTime >= (8+random.random()*15):
-        enemyChar.cooldown=DEFAULT_PUNCH_CD
+    if enemyChar.cooldown <= 0 and abs(enemyChar.xpos-youChar.xpos)<=ENEMY_TARGET_RANGE and youChar.positionTime >= (ENEMY_GODLINESS_BASE+random.random()+3):
         return PUNCH
     return REST
 
-def enemyPos(): 
-    funnevariable = enemyChar.xpos
-    if enemyChar.positionTime>=12+random.random()*13: #humble them with a reaction 
-        if abs(enemyChar.xpos-youChar.xpos)<=ENEMY_TARGET_RANGE: #if in same range currently
-            if youChar.cooldown< DEFAULT_PUNCH_CD * .4 and enemyChar.cooldown>=DEFAULT_PUNCH_CD*.5: #if it senses punch incoming and has been in space longer than reaction time
-                funnevariable+=(random.random()-.5)*.1 #move by a random amount in either direction
-        else:
-            if (enemyChar.cooldown < DEFAULT_PUNCH_CD *.4): #if punch cooldown is low and distance is far awayish then go closer
-                if (youChar.xpos<enemyChar.xpos):
-                    funnevariable-=.1
-                else:
-                    funnevariable+=.1
-    if (funnevariable>.5):
-        funnevariable =.5
-    elif funnevariable<-.5:
-        funnevariable=-.5
-    return funnevariable #maintain curr position
+
+
+
 
 def periodicEnemyPos(t):
     value = 0
@@ -115,23 +126,45 @@ def periodicEnemyPos(t):
         value += cos_amplitudes[i] * np.cos(cos_frequencies[i] * t)
 
     return value
+    #return enemyChar.xpos
 
 pygame.init()
-pygame.display.set_caption("Test")
+pygame.display.set_caption("XORcise")
 display_size = (1280, 720)
 screen = pygame.display.set_mode(display_size)
-font = pygame.font.Font("freesansbold.ttf", 32)
-# bg = pygame.image.load("bgbg.png")
-midimg = pygame.image.load("0_1_Idle.png")
-midimgsize = (midimg.get_width()//7,midimg.get_height()//7)
-midimg = pygame.transform.scale(midimg, midimgsize)
-leftimg = pygame.image.load("0_0_Idle.png")
-leftimgsize = (leftimg.get_width()//7,leftimg.get_height()//7)
-leftimg = pygame.transform.scale(leftimg, leftimgsize)
-rightimg = pygame.image.load("0_2_Idle.png")
-rightimgsize = (rightimg.get_width()//7,rightimg.get_height()//7)
-rightimg = pygame.transform.scale(rightimg, rightimgsize)
-        
+font32 = pygame.font.Font("freesansbold.ttf", 32)
+font24 = pygame.font.Font("freesansbold.ttf", 24)
+gamebg = pygame.image.load("bgbgbg.png")
+
+if difficulty == EASY:
+    midimg, midimgsize = image_load_downscale("0_1_Idle.png", 7)
+    mpg, mpgs = image_load_downscale("0_1_Punch.png", 7)
+    leftimg, leftimgsize = image_load_downscale("0_0_Idle.png", 7)
+    lpg, lpgs = image_load_downscale("0_0_Punch.png", 7)
+    rightimg, rightimgsize = image_load_downscale("0_2_Idle.png", 7)
+    rpg, rpgs = image_load_downscale("0_2_Punch.png", 7)
+elif difficulty == MEDIUM:
+    midimg, midimgsize = image_load_downscale("1_1_Idle.png", 7)
+    mpg, mpgs = image_load_downscale("1_1_Punch.png", 7)
+    leftimg, leftimgsize = image_load_downscale("1_0_Idle.png", 7)
+    lpg, lpgs = image_load_downscale("1_0_Punch.png", 7)
+    rightimg, rightimgsize = image_load_downscale("1_2_Idle.png", 7)
+    rpg, rpgs = image_load_downscale("1_2_Punch.png", 7)
+elif difficulty == HARD:
+    midimg, midimgsize = image_load_downscale("2_1_Idle.png", 7)
+    mpg, mpgs = image_load_downscale("2_1_Punch.png", 7)
+    leftimg, leftimgsize = image_load_downscale("2_0_Idle.png", 7)
+    lpg, lpgs = image_load_downscale("2_0_Punch.png", 7)
+    rightimg, rightimgsize = image_load_downscale("2_2_Idle.png", 7)
+    rpg, rpgs = image_load_downscale("2_2_Punch.png", 7)
+
+
+player_idle_1_img, player_idle_1_img_size = image_load_downscale("playeridle1.png", 12)
+player_idle_2_img, player_idle_2_img_size = image_load_downscale("playeridle2.png", 12)
+player_punch_img, player_punch_img_size = image_load_downscale("playerpunch.png", 12)
+
+hitimg, hitimgsize = image_load_downscale("hit.png", 1)
+missimg, missimgsize = image_load_downscale("miss.png", 1)
 
 DT_SHIFT = 10
 FPS = 60
@@ -140,45 +173,105 @@ gameClock = pygame.time.Clock()
 ticks = 0
 dt = 1
 punchAnimating = false
+enemypunching = false
+enemypunchanit = 0
 punchAnimatingt = 0
 
-uistate = GAME
+hitIcons = []
+missIcons = []
 
-def update(frameTime, uistate):
+uistate = MENU
+
+def update(frameTime):
+    
     # if isPunch():
     #     print("PUNCH!")
-    global ticks, punchAnimating, punchAnimatingt
+    global ticks, punchAnimating, punchAnimatingt, enemypunching, enemypunchanit, uistate
 
     dt = frameTime >> DT_SHIFT
     ticks += 1
     #playerpos = playerBodyPosition()
 
     if uistate == MENU:
+        text = font32.render("XORcise VIRTUAL BOXING", True, (255,0,0))
+        text2 = font24.render("Punch to get started!", True, (255,0,0))
+        textRect = text.get_rect()
+        text2Rect = text2.get_rect()
+        textRect.center = (display_size[0] // 2, display_size[1] // 2+90)
+        text2Rect.center = (display_size[0] // 2, display_size[1] // 2 + 150)
+        screen.blit(text, textRect)
+        screen.blit(text2, text2Rect)
         #update current state
+        #if currently calibrating, wait
+
         if (isPunch()): #TOO: add transition
             uistate=GAME
         #update ui
     elif uistate == GAME:
+        
+
+        
+        for i in hitIcons:
+            if (i.pos[1]>display_size[1] or i.pos[1]<0):
+                hitIcons.remove(i)
+                
+            else:
+                i.update()
+                i.display()
+
+        for i in missIcons:
+            if (i.pos[1]>display_size[1]):
+                missIcons.remove(i)
+            else:
+                i.update()
+                i.display()
         youChar.cooldown-=frameTime
         enemyChar.cooldown-=frameTime
         if  abs(enemyChar.xpos-youChar.xpos)<=ENEMY_TARGET_RANGE: #position time is checking how long they're within range now
             enemyChar.positionTime+=frameTime
             youChar.positionTime+=frameTime
+        else:
+            enemyChar.positionTime==0
+            youChar.positionTime==0
         youChar.update(playerPosition(), PUNCH if isPunch() else REST)
         enemyChar.update(periodicEnemyPos(ticks / difficulty), enemyAction())
         
         # print(enemyChar.xpos)
-        
         #hit detection
-        if  abs(enemyChar.xpos-youChar.xpos)<=ENEMY_TARGET_RANGE: #do the thingggggggg
+        if youChar.cooldown==ALLIED_COOLDOWN:
             if youChar.action == PUNCH:
-                enemyChar.healthPoints-=1
-                if enemyChar.healthPoints<=0:
-                    uiState=GAMEOVER
-            if enemyChar.action ==PUNCH:
-                youChar.healthPoints-=1
+                punchAnimating = True
+                punchAnimatingt = 50
+                if abs(enemyChar.xpos-youChar.xpos)<=ALLIED_HIT_RANGE:
+                    enemyChar.healthPoints-=1
+                    toAppend = physicsObj()
+                    print("hitobj")
+                    toAppend.setInit(hitimg,[(enemyChar.xpos+.5)*display_size[0],10+random.random()*15],[1+random.random()*2,-1 -random.random()], [0,1])
+                    hitIcons.append(toAppend)
+                    if enemyChar.healthPoints<=0:
+                        uiState=GAMEOVER
+            else:
+                toAppend = physicsObj()
+                # print("missobj")
+                toAppend.setInit(missimg,[(youChar.xpos+.5)*display_size[0],10+random.random()*15],[(random.random()-.5)*2.5,-1.25], [0,-1])
+                missIcons.append(toAppend)
+        if enemyChar.cooldown==ENEMY_COOLDOWN:
+            if enemyChar.action == PUNCH:
+                enemypunching = True
+                enemypunchanit = 50
+                if abs(enemyChar.xpos-youChar.xpos)<=ENEMY_TARGET_RANGE:
+                    youChar.healthPoints-=1
+                    # print("hitobj")
+                    toAppend = physicsObj()
+                    toAppend.setInit(hitimg,[(youChar.xpos+.5)*display_size[0],display_size[1]*.75+random.random()*5],[2*(random.random()-.5),-2+random.random()*1], [0,1+random.random()*.5])
+                    hitIcons.append(toAppend)
                 if youChar.healthPoints<=0:
-                    uiState=GAMEOVER
+                        uiState=GAMEOVER
+            else:
+                # print("missobj")
+                toAppend = physicsObj()
+                toAppend.setInit(missimg,[(enemyChar.xpos+.5)*display_size[0],display_size[1]*.75+random.random()*5],[1.5*(random.random()-.5),-1+random.random()*1], [0,1+random.random()*.5])
+                missIcons.append(toAppend)
         
         if youChar.action == PUNCH:
             punchAnimating = True
@@ -187,34 +280,60 @@ def update(frameTime, uistate):
             punchAnimatingt -= 1
         if punchAnimatingt <= 0:
             punchAnimating = False
+        if enemyChar.action == PUNCH:
+            enemypunching = True
+            enemypunchanit = 50
+        if enemypunching and enemypunchanit > 0:
+            enemypunchanit -= 1
+        if enemypunchanit <= 0:
+            enemypunching = False
 
         #key detection
 
         #update UI - bottom: HP
+        pygame.draw.rect(screen,(255,0,0),(display_size[0]*.1,0, display_size[0]*.8*(enemyChar.healthPoints)/ENEMY_HP,display_size[1]*.05))
+        
+        pygame.draw.rect(screen,(0,0,255),(0,display_size[1]*.2, display_size[0]*.05,display_size[1]*(max(0,youChar.cooldown)/ALLIED_COOLDOWN)))
+
+        pygame.draw.rect(screen,(0,255,0),(display_size[0]*.1,display_size[1]*.90, display_size[0]*.8*(youChar.healthPoints)/ALLIED_HP,display_size[1]*.1)) #your healthbar
         # print(youChar.cooldown, enemyChar.cooldown, youChar.xpos, enemyChar.xpos, asdp)
+        # print(youChar.healthPoints, enemyChar.healthPoints, youChar.cooldown, enemyChar.cooldown)
         # pygame.draw.circle(screen, (255,0,0), (enemyChar.xpos * display_size[0] + (display_size[0] / 2), 200), 25) #may not work
         if enemyChar.xpos < -0.1:
-            screen.blit(leftimg, (enemyChar.xpos * display_size[0] + (display_size[0] / 2) - leftimgsize[0]/2, 250 - leftimgsize[1]/2 + (20+random.random()*10)*np.sin(ticks/7)))
+            if enemypunching:
+                screen.blit(lpg, (enemyChar.xpos * display_size[0] + (display_size[0] / 2) - lpgs[0]/2, 250 - lpgs[1]/2 + (20+random.random()*10)*np.sin(ticks/7)))
+            else:
+                screen.blit(leftimg, (enemyChar.xpos * display_size[0] + (display_size[0] / 2) - leftimgsize[0]/2, 250 - leftimgsize[1]/2 + (20+random.random()*10)*np.sin(ticks/7)))
         elif enemyChar.xpos > 0.1:
-            screen.blit(rightimg, (enemyChar.xpos * display_size[0] + (display_size[0] / 2) - rightimgsize[0]/2, 250 - rightimgsize[1]/2 + (20+random.random()*10)*np.sin(ticks/7)))
+            if enemypunching:
+                screen.blit(rpg, (enemyChar.xpos * display_size[0] + (display_size[0] / 2) - rpgs[0]/2, 250 - rpgs[1]/2 + (20+random.random()*10)*np.sin(ticks/7)))
+            else:
+                screen.blit(rightimg, (enemyChar.xpos * display_size[0] + (display_size[0] / 2) - rightimgsize[0]/2, 250 - rightimgsize[1]/2 + (20+random.random()*10)*np.sin(ticks/7)))
         else:
-            screen.blit(midimg, (enemyChar.xpos * display_size[0] + (display_size[0] / 2) - midimgsize[0]/2, 250 - midimgsize[1]/2 + (20+random.random()*10)*np.sin(ticks/7)))
+            if enemypunching:
+                screen.blit(mpg, (enemyChar.xpos * display_size[0] + (display_size[0] / 2) - mpgs[0]/2, 250 - mpgs[1]/2 + (20+random.random()*10)*np.sin(ticks/7)))
+            else:
+                screen.blit(midimg, (enemyChar.xpos * display_size[0] + (display_size[0] / 2) - midimgsize[0]/2, 250 - midimgsize[1]/2 + (20+random.random()*10)*np.sin(ticks/7)))
         
-        pygame.draw.circle(screen, (0,255,0), (youChar.xpos * display_size[0] + (display_size[0] / 2), 600), 25)
+        # pygame.draw.circle(screen, (0,255,0), (youChar.xpos * display_size[0] + (display_size[0] / 2), 600), 25)
         if punchAnimating:
-            pygame.draw.rect(screen, (0,0,255), (0,display_size[1]-75, display_size[0],75))
+            screen.blit(player_punch_img, (youChar.xpos * display_size[0] + (display_size[0] / 2) - player_punch_img_size[0]/2, 600 - player_punch_img_size[1]/2))
+        elif youChar.xpos <= 0:
+            screen.blit(player_idle_1_img, (youChar.xpos * display_size[0] + (display_size[0] / 2) - player_idle_1_img_size[0]//2, 600 - player_idle_1_img_size[1]/2))
+        else:
+            screen.blit(player_idle_2_img, (youChar.xpos * display_size[0] + (display_size[0] / 2) - player_idle_2_img_size[0]//2, 600 - player_idle_2_img_size[1]/2))
 
     elif uistate == GAMEOVER:
         #display gameover screen
-
-        if isPunch:
-            uiState=MENU
-    
+        pygame.draw.rect(screen,(255,255,255),(0,0,display_size[0],display_size[1]))
+        
+        #if isPunch():
+        #    uiState=MENU
     else:
         pass #noop
 while True:
     screen.fill((0,0,0))
-    # screen.blit(bg, (0,0))
+    screen.blit(gamebg, (0,0))
     asdp[0] = False; asdp[1] = False; asdp[2] = False; asdp[3] = False
     for event in pygame.event.get():
         if event.type == QUIT:
@@ -230,5 +349,5 @@ while True:
             if event.key == K_p:
                 asdp[3] = True
     frameTime = gameClock.tick(FPS)
-    update(dt, uistate)
+    update(dt)
     pygame.display.flip()
--- util.py
+++ util.py
@@ -1,13 +1,35 @@
+import pygame
+
 LEFT = 0
 RIGHT = 2
 MIDDLE = 1
 
 REST = "rest"
 PUNCH = "punch"
-DEFAULT_PUNCH_CD = 5
-DEFAULT_HP = 10
+
+ENEMY_COOLDOWN = 150
+ALLIED_COOLDOWN = 75
+ENEMY_HP = 5
+ALLIED_HP = 7
 ENEMY_TARGET_RANGE = .2
+ALLIED_HIT_RANGE = .3
+TICK_PHYSICS_MULTIPLIER = 500
+
+ENEMY_GODLINESS_BASE =  20 #higher is easier
+
 
 MENU = "menu"
 GAME = "game"
 GAMEOVER = "gameover"
+
+EASY = 2000
+MEDIUM = 1300
+HARD = 700
+BOSS = 500
+
+def image_load_downscale(name, factor):
+    img = pygame.image.load(name)
+    imgsize = (img.get_width()//factor,img.get_height()//factor)
+    img = pygame.transform.scale(img, imgsize)
+
+    return (img, imgsize)
